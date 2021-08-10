ab1034cd92a61b15ec18f19d3cac0e269be43326
Raimonds Kazuss <rkazush@gmail.com>
2021-08-10 05:41:29 +0300

    concise video finished code

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
-
 const express = require('express')
-const server = express()
+const app = express()
 const bcrypt = require('bcrypt')
 
-server.use(express.json())
+app.use(express.json())
 
-const users = []
+const users = [];
 
-server.get('/users', (req, res) => {
-  res.json(users)
+// get all users
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
-// unencrypted
-server.post('/users', async (req, res) => {
+// create a new user
+app.post('/users', async (req, res) => {
     try {
-      const user = { name: req.body.name, password: req.body.password }
-      users.push(user)
-      res.status(201).send()
+        
+        // crypt.hash(string, saltRounds)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send()
     } catch {
-      res.status(500).send()
+        res.status(500).send()
     }
-  })
-
-// encrypted  
-// app.post('/users', async (req, res) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//     const user = { name: req.body.name, password: hashedPassword }
-//     users.push(user)
-//     res.status(201).send()
-//   } catch {
-//     res.status(500).send()
-//   }
-// })
-
-server.post('/users/login', async (req, res) => {
-  const user = users.find(user => user.name === req.body.name)
-  if (user == null) {
-    return res.status(400).send('Cannot find user')
-  }
-  try {
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-      res.send('Not Allowed')
-    }
-  } catch {
-    res.status(500).send()
-  }
 })
 
-server.listen(3000)
+
+// login a user
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (!user) {
+        return res.status(400).send('Sorry something went wrong!')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success!')
+        } else {
+            res.send('Wrong password!')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`server running ar http://localhost:${3000}`);
+})
